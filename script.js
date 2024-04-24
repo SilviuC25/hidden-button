90ea67e5839fc7839c79227b2225a560ef1e1d2e
VilutSilviu <vilut.chisalita@smartboxdigital.com>
2024-04-24 08:37:36 +0200

    Game Functionalities and Some Styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let startButton = document.getElementById("btn-start");
 let inputGroup = document.getElementById("input-group");
 let buttonsContainer = document.getElementById("buttons-container");
+let endGameText = document.getElementById('text-end');
 
 function createInput() {
     inputGroup.classList.remove("visually-hidden");
@@ -19,7 +20,7 @@ function generateButtons() {
         alertText.classList.add("visually-hidden");
     }
 
-    buttonsContainer.innerHTML = '';
+    buttonsContainer.innerHTML = ''; // the container must be emptied before generating the new buttons
 
     for (let buttonIndex = 0; buttonIndex < numberOfButtons; ++buttonIndex) {
         let button = document.createElement('button');
@@ -30,7 +31,40 @@ function generateButtons() {
     startGame();
 }
 
-let wrongTries = 0;
+let wrongTries;
+let correctButton;
+
 function startGame() {
+    let buttons = buttonsContainer.querySelectorAll('button');
+    let numberOfButtons = buttons.length;
+    let randomIndex = Math.floor(Math.random() * numberOfButtons);
+    correctButton = buttons[randomIndex];
+
+    buttons.forEach(button => {
+        button.addEventListener('click', buttonClickHandler);
+    });
     
+    wrongTries = 0;
+    endGameText.classList.add('visually-hidden');
+}
+
+function buttonClickHandler(event) {
+    let clickedButton = event.target;
+    clickedButton.classList.remove('btn-info');
+
+    if (clickedButton === correctButton) {
+        clickedButton.classList.add('btn-success');
+        endGame();
+    } else {
+        clickedButton.classList.add('btn-danger');
+        ++wrongTries;
+    }
+}
+
+function endGame() {
+    let buttons = buttonsContainer.querySelectorAll('button');
+    let numberOfButtons = buttons.length;
+    let score = numberOfButtons - wrongTries;
+    endGameText.textContent = `Your score is ${score} out of ${numberOfButtons}`;
+    endGameText.classList.remove('visually-hidden');
 }
